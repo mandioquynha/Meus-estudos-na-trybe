255fa76a42490c732a098bdd5a90a82d30218fa6
Your Name <you@example.com>
2021-11-29 20:53:29 -0300

    Retorne o número par de três números usando apenas um if.

--- fundamentos/bloco-04-itroducao-a-javascript-e-logica-de-programacao/exercicio-8/script.js
+++ fundamentos/bloco-04-itroducao-a-javascript-e-logica-de-programacao/exercicio-8/script.js
@@ -1,11 +1,11 @@
 /*Escreva um programa que defina três números em constantes e retorne true se pelo menos uma das três for par. Caso contrário, ele retorna false .
 Bonus: use somente um if .*/
 
-const a = 6;
+const a = 8;
 const b = 3;
 const c = 9;
 
-if ((a / a) === 1 || (b / b) === 1 || (c / c) === 1) {
+if ((a % 2) === 0 || (b %2 ) === 0 || (c % 2) === 0) {
 console.log('true');
 } else {
     console.log('false');
