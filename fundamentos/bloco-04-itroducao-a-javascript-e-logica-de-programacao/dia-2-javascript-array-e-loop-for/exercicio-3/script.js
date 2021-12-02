7105d7f4cf76c39d5e3d0d60e997514abc40d973
Your Name <you@example.com>
2021-12-01 22:19:34 -0300

    Média aritmética dos valores de um array

---
+++ fundamentos/bloco-04-itroducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/exercicio-3/script.js
@@ -0,0 +1,9 @@
+//Para o terceiro exercício, calcule e imprima a média aritmética dos valores contidos no array;
+//A média aritmética é o resultado da soma de todos os elementos divido pelo número total de elementos.
+
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let soma = 0;
+for(var index = 0; index < numbers.length; index +=1) {
+   soma += numbers[index]
+}
+console.log(soma / numbers.length);
