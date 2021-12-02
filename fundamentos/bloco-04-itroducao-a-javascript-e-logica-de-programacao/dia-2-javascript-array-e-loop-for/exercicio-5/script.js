53fa3fdac23173ba05aa1a8ca6692af140f3def3
Your Name <you@example.com>
2021-12-01 23:47:14 -0300

    Encontrando o maior número em um array numérico

--- fundamentos/bloco-04-itroducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/exercicio-5/script.js
+++ fundamentos/bloco-04-itroducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/exercicio-5/script.js
@@ -1,9 +1,8 @@
-//Utilizando for , descubra qual o maior valor contido no array e imprima-o;
-
-let impar;
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let numeroMaior = 0;
 for(var index = 0; index < numbers.length; index +=1) {
-   if (numbers[index] % 2 !== 0) {
-       console.log(numbers[index]);
-   }
+   if (numeroMaior < numbers[index]) {
+       numeroMaior = numbers[index]
+    }
 }
+   console.log(numeroMaior);
